0580a61cc9086c21574a105c02812f6aaa9ee6e1
saurabhtiwaridev <s.t.48826@gmail.com>
2024-07-22 22:19:28 +0530

    added the productController into our app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import productController from "./src/controllers/products.controller.js";
 const port = 3100;
 
 const app = express();
+const productControllerClass = new productController();
 
-app.get("/", (req, res) => {
-  return res.send("App initiated");
-});
+app.use(express.static("src/views"));
+
+app.get("/", productControllerClass.getProduct);
 
 app.listen(port, () => {
   console.log("server is up & listening on port", port);
